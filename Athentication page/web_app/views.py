0f02283c052cd47c585eae51639fe4c98d234827
malik <malik@kali>
2024-01-13 10:58:39 +0500

    Website with Login & Registration page

--- Athentication page/web_app/views.py
+++ Athentication page/web_app/views.py
@@ -1,16 +1,49 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+import requests
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
 def index(req):
-    return render(req, '/home/malik/test_website/Athentication page/web_app/templates/web_app/index.html')
+    return render(req, 'web_app/index.html')
 
-def login(req):
-    return render(req, '/home/malik/test_website/Athentication page/web_app/templates/web_app/login.html')
+def login_view(req):
+    if req.method=='POST':
+        Email=req.POST.get('username')
+        Pass=req.POST.get('psw')
 
-def logout(req):
+        user = authenticate(req, username=Email, password=Pass)
+        
+        if user is not None:
+            login(req, user)
+            return HttpResponse("Login successful!")
+        else:
+            return HttpResponse("Sorry! try again")
+
+    return render(req, 'web_app/login.html')
+
+def logout_view(req):
     return HttpResponse('Logout page')
 
 def register(req):
-    return render(req, '/home/malik/test_website/Athentication page/web_app/templates/web_app/register.html')
+    if req.method=='POST':
+        Email=req.POST.get('username')
+        Pass=req.POST.get('psw')
+        Pass1=req.POST.get('psw-repeat')
+
+        user=User.objects.create_user(Email, Pass, Pass1)
+        user.save()
+        return HttpResponse("user has been created successfully")
+        return redirect('login')
+
+
+
+
+
+
+
+
+
+    return render(req, 'web_app/register.html')
 
